521786e65312b8dab2f75fd66d2788527d5a3384
lordlabuckdas <55460753+lordlabuckdas@users.noreply.github.com>
2022-10-31 12:04:26 +0530

    [nvim] update nvim-cmp

--- nvim/lua/cmp_conf.lua
+++ nvim/lua/cmp_conf.lua
@@ -24,7 +24,7 @@ cmp.setup({
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
+        ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
         ['<Tab>'] = function(fallback)
             if cmp.visible() then
@@ -63,6 +63,6 @@ local servers = {
 
 for _, lsp in ipairs(servers) do
     require('lspconfig')[lsp].setup {
-        capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+        capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
     }
 end
